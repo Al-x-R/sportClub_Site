7bb128751edcf650eb7afbde96f45f501b7afade
Al-x-R <alexander.roshchin85@gmail.com>
2020-01-18 22:49:30 +0200

    pages updated

--- maximus/pages/views.py
+++ maximus/pages/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from sections.models import Section
+from trainers.models import Trainer
 
 # Create your views here.
+def index(request):
+    #return HttpResponse('<h1>Hello</h1>')
+    sections = Section.objects.order_by('title').filter(is_published=True)
+    trainers = Trainer.objects.order_by('name')
+    context = {
+        'sections': sections,
+        'trainers': trainers,
+    }
+    return render(request, 'pages/index.html', context) #
+
+
+def about(request):
+    ## Get all realtors
+    # realtors = Realtor.objects.order_by('-hire_date')
+    #
+    # # Get MVP
+    # mpv_realtors = Realtor.objects.all().filter(is_mpv=True)
+    # context = {
+    #     'realtors': realtors,
+    #     'mvp_realtors': mpv_realtors
+    # }
+    # return render(request, 'pages/about.html', context)
+    pass
